6efd746cd0d441314ab64bf22938fd32aead5fb9
zhangqiqian <zhangqiqian2012@gmail.com>
2015-04-21 00:00:19 +0800

    del comment.

--- OneLibrary/app/src/main/java/org/onelibrary/MainActivity.java
+++ OneLibrary/app/src/main/java/org/onelibrary/MainActivity.java
@@ -1,12 +1,9 @@
 package org.onelibrary;
 
 
-import android.app.Activity;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -17,26 +14,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.onelibrary.data.DatabaseAdapter;
 import org.onelibrary.data.MessageCollection;
-import org.onelibrary.data.MessageItem;
 import org.onelibrary.util.NetworkAdapter;
 
 import java.io.IOException;
-import java.lang.annotation.Target;
-import java.sql.SQLException;
 
-public class MainActivity extends FragmentActivity {//implements AdapterView.OnItemClickListener {
+public class MainActivity extends FragmentActivity {
 
     public static final String TAG = "MainActivity";
 
@@ -46,8 +33,6 @@ public class MainActivity extends FragmentActivity {//implements AdapterView.OnI
     public final static String IS_LOGIN = "is_login";
     public final static String USERNAME = "username";
     public final static String PASSWORD = "password";
-
-    private MessageCollection messages = null;
 
 
     @Override
@@ -93,62 +78,11 @@ public class MainActivity extends FragmentActivity {//implements AdapterView.OnI
 
 
 
-    private void showListView(){
-        /*ListView itemList = (ListView) findViewById(R.id.message_list);
-        if (messages == null) {
-            return;
-        }
-        SimpleAdapter adapter = new SimpleAdapter(this, messages.getAllItemsForListView(), android.R.layout.simple_list_item_2, new String[]{MessageItem.TITLE,MessageItem.PUBDATE}, new int[]{android.R.id.text1,android.R.id.text2});
-        itemList.setAdapter(adapter);
-        itemList.setOnItemClickListener(this);
-        itemList.setSelection(0);*/
-    }
-
-    /*@Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        Bundle bundle = new Bundle();
-        MessageItem item = messages.getMessageItem(position);
-        bundle.putInt("id", item.getId());
-        bundle.putInt("message_id", item.getMessageId());
-        bundle.putString("title", item.getTitle());
-        bundle.putString("author", item.getAuthor());
-        bundle.putString("content", item.getContent());
-        bundle.putString("category", item.getCategory());
-        bundle.putString("link", item.getLink());
-        bundle.putString("tags", item.getTags());
-        bundle.putString("pubdate", item.getPubdate());
-
-        intent.putExtra("message_item", bundle);
-        startActivityForResult(intent, 0);
-
-    }*/
-
-    /*@Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        Log.i("MainActivity", "query: " + intent.getAction());
-        Log.i("MainActivity", "query: " + Intent.ACTION_SEARCH);
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //use the query to search your data somehow
-            Log.i("MainActivity", "query: " + query);
-        }
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        //Associate searchable configuration with the SearchView
-        /*SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));*/
         return true;
     }
 
@@ -176,7 +110,6 @@ public class MainActivity extends FragmentActivity {//implements AdapterView.OnI
 
         return super.onOptionsItemSelected(item);
     }
-
 
     /**
      * Implementation of AsyncTask, to fetch the data in the background away from
