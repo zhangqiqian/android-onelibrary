887c3e577fbe3a52dcbda77b530bf071527e0b1e
zhangqiqian <zhangqiqian2012@gmail.com>
2015-04-21 00:18:23 +0800

    del comment.

--- OneLibrary/app/src/main/java/org/onelibrary/DetailActivity.java
+++ OneLibrary/app/src/main/java/org/onelibrary/DetailActivity.java
@@ -1,17 +1,11 @@
 package org.onelibrary;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Paint;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
 
 public class DetailActivity extends Activity {
 
@@ -20,14 +14,6 @@ public class DetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        //assert if network is ok
-        /*ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if(networkInfo == null){
-            Toast.makeText(DetailActivity.this, "Unconnected to network.", Toast.LENGTH_SHORT).show();
-        }*/
-
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setDisplayShowTitleEnabled(false);
         Intent intent = getIntent();
 
@@ -50,7 +36,6 @@ public class DetailActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -59,12 +44,12 @@ public class DetailActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
