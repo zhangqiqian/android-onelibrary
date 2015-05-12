d538b332a4ddc62b72907620e1a731050aa06630
zhangqiqian <zhangqiqian2012@gmail.com>
2015-05-13 05:07:56 +0800

    modify location activity

--- OneLibrary/app/src/main/java/org/onelibrary/LocationActivity.java
+++ OneLibrary/app/src/main/java/org/onelibrary/LocationActivity.java
@@ -2,6 +2,7 @@ package org.onelibrary;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,6 +10,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -53,6 +56,9 @@ public class LocationActivity extends Activity {
         }
     };
 
+    Button btnShowLocation;
+    LocationService gps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +78,31 @@ public class LocationActivity extends Activity {
             Toast.makeText(getBaseContext(), "Location is disabled.", Toast.LENGTH_LONG).show();
         }
 
+        btnShowLocation = (Button) findViewById(R.id.locationBtn);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new LocationService(LocationActivity.this);
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
     }
 
     @Override
