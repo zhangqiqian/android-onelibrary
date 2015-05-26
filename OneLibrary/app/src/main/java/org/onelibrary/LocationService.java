26d3684302ec778794871e79c04b8444cd694dfd
zhangqiqian <zhangqiqian2012@gmail.com>
2015-05-26 23:23:21 +0800

    location dialog.

--- OneLibrary/app/src/main/java/org/onelibrary/LocationService.java
+++ OneLibrary/app/src/main/java/org/onelibrary/LocationService.java
@@ -174,13 +174,13 @@ public class LocationService extends Service implements LocationListener {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
         // Setting Dialog Title
-        alertDialog.setTitle("GPS is settings");
+        alertDialog.setTitle(R.string.location_alert_title);
 
         // Setting Dialog Message
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+        alertDialog.setMessage(R.string.location_alert_content);
 
         // On pressing Settings button
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+        alertDialog.setPositiveButton(R.string.location_alert_settings_btn, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 mContext.startActivity(intent);
@@ -188,7 +188,7 @@ public class LocationService extends Service implements LocationListener {
         });
 
         // on pressing cancel button
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        alertDialog.setNegativeButton(R.string.location_alert_cancel_btn, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
