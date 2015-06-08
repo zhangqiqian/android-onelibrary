9b9d6f32106f2a0d60b27191ef70baa7235f47ee
zhangqiqian <zhangqiqian2012@gmail.com>
2015-06-09 03:44:59 +0800

    add get remote messages method.

--- OneLibrary/app/src/main/java/org/onelibrary/SchedulingService.java
+++ OneLibrary/app/src/main/java/org/onelibrary/SchedulingService.java
@@ -9,12 +9,11 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.onelibrary.data.DbAdapter;
+import org.onelibrary.data.MessageDataManager;
+import org.onelibrary.data.MessageItem;
+
+import java.util.List;
 
 /**
  * This {@code IntentService} does the app's actual work.
@@ -46,47 +45,48 @@ public class SchedulingService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i("SchedulingService", "------onHandleIntent------");
-        fetchNewItems();
+        LocationService locationService = new LocationService(getBaseContext());
+        double longitude = locationService.getLongitude();
+        double latitude  = locationService.getLatitude();
+
+        DbAdapter mDbAdapter = new DbAdapter(getBaseContext());
+        MessageDataManager manager = new MessageDataManager(mDbAdapter);
+        List<MessageItem> messageItems = manager.getRemoteMessages(getBaseContext(), longitude, latitude);
+
+        int size = messageItems.size();
+        String content = "New Messages.";
+        for (MessageItem item : messageItems){
+            if(mDbAdapter.messageIsExist(item)){
+                size--;
+            }else{
+                manager.addMessage(item);
+                content = item.getTitle();
+            }
+        }
+
+        Log.i("SchedulingService", "------ send notification ------");
+        String msg = "You have new messages.";
+        String title = "New Notification";
+        if (size > 0) {
+            if(size == 1){
+                int notification_id = (int)(Math.random() * 10 + 1);
+                msg = content;
+                sendNotification(notification_id, title, msg);
+            }else{
+                msg = "You have " + size + "new messages.";
+                sendNotification(NOTIFICATION_ID, title, msg);
+            }
+            Log.i(TAG, msg);
+        }
+
         // Release the wake lock provided by the BroadcastReceiver.
         AlarmReceiver.completeWakefulIntent(intent);
         // END_INCLUDE(service_onhandle)
     }
 
-    /**
-     * Handle action Bar in the provided background thread with the provided
-     * parameters.
-     */
-    private void fetchNewItems() {
-        // BEGIN_INCLUDE(service_onhandle)
-        // The URL from which to fetch content.
-        String urlString = URL + "/s?wd=" + SEARCH_STRING;
-
-        String result ="";
-
-        // Try to connect to the Google homepage and download content.
-        try {
-            Log.i("SchedulingService", "------handleActionFetchNewItems url "+URL+" ------");
-
-            result = loadFromNetwork(urlString);
-        } catch (IOException e) {
-            Log.i(TAG, getString(R.string.connection_error));
-        }
-
-        // If the app finds the string "doodle" in the Google home page content, it
-        // indicates the presence of a doodle. Post a "Doodle Alert" notification.
-        Log.i("SchedulingService", "------will sendNotification------");
-        if (result.contains(SEARCH_STRING)) {
-            String msg = "Found doodle, length: " +result.length()+ ".";
-            sendNotification(msg);
-            Log.i(TAG, msg);
-        } else {
-            sendNotification("No doodle found. :-(");
-            Log.i(TAG, "No doodle found. :-(");
-        }
-    }
 
     // Post a notification indicating whether a doodle was found.
-    private void sendNotification(String msg) {
+    private void sendNotification(int notification_id, String title, String msg) {
         mNotificationManager = (NotificationManager)
                this.getSystemService(Context.NOTIFICATION_SERVICE);
     
@@ -98,76 +98,16 @@ public class SchedulingService extends IntentService {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
         .setSmallIcon(R.drawable.ic_launcher)
-        .setContentTitle("New Notification.")
+        .setContentTitle(title)
         .setStyle(new NotificationCompat.BigTextStyle()
         .bigText(msg))
-        .setContentText(msg).setAutoCancel(true).setDefaults(Notification.DEFAULT_LIGHTS);
+        .setContentText(msg).setAutoCancel(true).setDefaults(Notification.DEFAULT_ALL);
 
         mBuilder.setContentIntent(contentIntent);
         Notification notification = mBuilder.build();
-        //notification.defaults = Notification.DEFAULT_SOUND;
-        mNotificationManager.notify(NOTIFICATION_ID, notification);
+        mNotificationManager.notify(notification_id, notification);
     }
  
-//
-// The methods below this line fetch content from the specified URL and return the
-// content as a string.
-//
-    /** Given a URL string, initiate a fetch operation. */
-    private String loadFromNetwork(String urlString) throws IOException {
-        InputStream stream = null;
-        String str ="";
-
-        Log.i("SchedulingService", "------loadFromNetwork------");
-
-        try {
-            stream = downloadUrl(urlString);
-            str = readIt(stream);
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }      
-        }
-        return str;
-    }
-
-    /**
-     * Given a string representation of a URL, sets up a connection and gets
-     * an input stream.
-     * @param urlString A string representation of a URL.
-     * @return An InputStream retrieved from a successful HttpURLConnection.
-     * @throws IOException
-     */
-    private InputStream downloadUrl(String urlString) throws IOException {
-    
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Start the query
-        conn.connect();
-        InputStream stream = conn.getInputStream();
-        return stream;
-    }
-
-    /** 
-     * Reads an InputStream and converts it to a String.
-     * @param stream InputStream containing HTML from www.google.com.
-     * @return String version of InputStream.
-     * @throws IOException
-     */
-    private String readIt(InputStream stream) throws IOException {
-      
-        StringBuilder builder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        for(String line = reader.readLine(); line != null; line = reader.readLine()) 
-            builder.append(line);
-        reader.close();
-        return builder.toString();
-    }
-
     @Override
     public void onDestroy() {
         Intent intent = new Intent("org.onelibrary.scheduling.destroy");
