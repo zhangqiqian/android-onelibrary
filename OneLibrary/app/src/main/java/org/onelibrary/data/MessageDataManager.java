971ec79dea5fa0e1b637c19765d65e8a6e73c97c
zhangqiqian <zhangqiqian2012@gmail.com>
2015-06-09 03:48:46 +0800

    add get_remote_messages.

--- OneLibrary/app/src/main/java/org/onelibrary/data/MessageDataManager.java
+++ OneLibrary/app/src/main/java/org/onelibrary/data/MessageDataManager.java
@@ -16,7 +16,20 @@
 
 package org.onelibrary.data;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.onelibrary.R;
+import org.onelibrary.util.NetworkAdapter;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -25,7 +38,17 @@ import java.util.List;
  */
 public class MessageDataManager {
 
+    private static final String LOG_TAG = "MessageDataManager";
+
     private DbAdapter mDbAdapter;
+
+    public final static String MAIN_INFO = "main_info";
+    public final static String REQUEST_LAST_TIME = "last_time";
+    public final static String REQUEST_LAST_PUBLISH_ID = "last_publish_id";
+    public final static String REQUEST_LAST_LONGITUDE = "longitude";
+    public final static String REQUEST_LAST_LATITUDE = "latitude";
+    public final static String REQUEST_NEXT_START = "next_start";
+
 
     private final List<MessageItem> mMessagesList = new ArrayList<MessageItem>();
 
@@ -105,5 +128,68 @@ public class MessageDataManager {
                 }
             }
         }
+    }
+
+    public List<MessageItem> getRemoteMessages(Context ctx, double longitude, double latitude){
+        List<MessageItem> messageItems = new ArrayList<MessageItem>();
+        try {
+            NetworkAdapter adapter = new NetworkAdapter(ctx);
+            SharedPreferences session = ctx.getSharedPreferences(MAIN_INFO, 0);
+            long last_time = session.getLong(REQUEST_LAST_TIME, 0);
+            long last_publish_id = session.getLong(REQUEST_LAST_PUBLISH_ID, 0);
+            int next_start = session.getInt(REQUEST_NEXT_START, 0);
+
+            long new_last_time = System.currentTimeMillis()/1000;
+            if(new_last_time - last_time > 60){
+                next_start = 0;
+            }
+
+            Bundle params = new Bundle();
+            params.putString(REQUEST_LAST_TIME, String.valueOf(last_time));
+            params.putString(REQUEST_LAST_PUBLISH_ID, String.valueOf(last_publish_id));
+            params.putString(REQUEST_LAST_LONGITUDE, String.valueOf(longitude));
+            params.putString(REQUEST_LAST_LATITUDE, String.valueOf(latitude));
+            params.putString(REQUEST_NEXT_START, String.valueOf(next_start));
+
+            Log.i(LOG_TAG, "Request params: " + params.toString());
+            try{
+                JSONObject result = adapter.request(ctx.getString(R.string.get_messages_url), params);
+
+                if(result.getInt("errno") == 0){
+                    Log.i(LOG_TAG, "success to get messages");
+
+                    int start = result.getInt("start");
+
+                    JSONArray messagesArray = result.getJSONArray("result");
+                    if(messagesArray.length() > 0){
+                        for (int i = 0;i< messagesArray.length();i++){
+                            JSONObject message = messagesArray.getJSONObject(i);
+                            MessageItem item = new MessageItem();
+                            item.setPublishId(message.getInt("publish_id"));
+                            item.setMessageId(message.getInt("message_id"));
+                            item.setTitle(message.getString("title"));
+                            item.setAuthor("");
+                            item.setContent("");
+                            item.setCategory("");
+                            item.setTags("");
+                            item.setLink("");
+                            item.setPubdate("");
+                            item.setStatus(0);
+                            item.setCtime(Calendar.getInstance());
+                            messageItems.add(item);
+                        }
+                        session.edit().putInt(REQUEST_NEXT_START, start).putLong(REQUEST_LAST_TIME, new_last_time).apply();
+                        Log.i(LOG_TAG, "new_last_time=" + new_last_time + " start=" + start);
+                    }
+                }else{
+                    Log.i(LOG_TAG, "failure: " + result.getString("errmsg"));
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return messageItems;
     }
 }
