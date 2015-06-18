5f16bd20349639bbd9d74bb3c18aeeaab7c1b923
zhangqiqian <zhangqiqian2012@gmail.com>
2015-06-18 23:17:11 +0800

    del useless code.

--- OneLibrary/app/src/main/java/org/onelibrary/SettingsActivity.java
+++ OneLibrary/app/src/main/java/org/onelibrary/SettingsActivity.java
@@ -2,7 +2,6 @@ package org.onelibrary;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
