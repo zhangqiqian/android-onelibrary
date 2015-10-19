3d28c36943baa014e1e05b1b1192b6c43686ce40
zhangqiqian <zhangqiqian2012@gmail.com>
2015-10-19 22:45:39 +0800

    modify params in get remote messages.

--- OneLibrary/app/src/main/java/org/onelibrary/LocationService.java
+++ OneLibrary/app/src/main/java/org/onelibrary/LocationService.java
@@ -49,7 +49,7 @@ public class LocationService extends Service implements LocationListener {
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -291,7 +291,7 @@ public class LocationService extends Service implements LocationListener {
         protected List<MessageItem> doInBackground(Double...params) {
             //get remote message, and save to db.
             MessageDataManager manager = new MessageDataManager(mContext);
-            return manager.getRemoteMessages(mContext, params[0], params[1]);
+            return manager.getRemoteMessages(mContext, params[0], params[1], 1, 10);
         }
 
         @Override
