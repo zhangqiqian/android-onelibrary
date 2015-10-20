6ef242b139e9f3c76dfba1793e1c153d893bb14c
zhangqiqian <zhangqiqian2012@gmail.com>
2015-10-20 11:05:19 +0800

    add getDistance method.

--- OneLibrary/app/src/main/java/org/onelibrary/data/LocationDataManager.java
+++ OneLibrary/app/src/main/java/org/onelibrary/data/LocationDataManager.java
@@ -120,4 +120,23 @@ public class LocationDataManager {
         }
         return bdLocation;
     }
+
+    public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2)
+    {
+        double EARTH_RADIUS = 6378.137;
+        double radLat1 = rad(latitude1);
+        double radLat2 = rad(latitude2);
+        double a = radLat1 - radLat2;
+        double b = rad(longitude1) - rad(longitude2);
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s * 1000;
+    }
+
+    public static double rad(double d)
+    {
+        return d * 3.1415926535898 / 180.0;
+    }
+
 }
