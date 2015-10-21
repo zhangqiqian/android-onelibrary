e8b35a1394c9096a7f45d29e84159353fa241de7
zhangqiqian <zhangqiqian2012@gmail.com>
2015-10-21 16:12:32 +0800

    restore to last version.

--- OneLibrary/app/src/main/java/org/onelibrary/data/LocationDataManager.java
+++ OneLibrary/app/src/main/java/org/onelibrary/data/LocationDataManager.java
@@ -130,7 +130,7 @@ public class LocationDataManager {
         double b = rad(longitude1) - rad(longitude2);
         double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
         s = s * EARTH_RADIUS;
-        s = Math.round(s * 10000) / 10000.0;
+        s = Math.round(s * 10000 / 10000.0);
         return s * 1000;
     }
 
--- OneLibrary/app/src/main/java/org/onelibrary/data/MessageDataManager.java
+++ OneLibrary/app/src/main/java/org/onelibrary/data/MessageDataManager.java
@@ -164,7 +164,7 @@ public class MessageDataManager {
 
             //todo save every bd location, will be deleted after release
             locationDataManager = new LocationDataManager(ctx);
-            LocationEntry entry = new LocationEntry("BD Location", bdLon, bdLat, Calendar.getInstance());
+            LocationEntry entry = new LocationEntry("Location", bdLon, bdLat, Calendar.getInstance());
             locationDataManager.addPoint(entry);
 
             //get messages
